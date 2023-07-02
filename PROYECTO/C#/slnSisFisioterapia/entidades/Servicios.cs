14c8808e381b6b00e54731269ceeba9226b63638
enciso <encisochezn@gmail.com>
2023-07-02 17:30:39 -0500

    puntito

---
+++ PROYECTO/C#/slnSisFisioterapia/entidades/Servicios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entidades
+{
+    public class Servicios
+    {
+        [Key]
+        public string IdServ { get; set; } = null!;
+        public string servicio { get; set; } = null!;
+        public string precio { get; set; } = null!;
+    }
+}
