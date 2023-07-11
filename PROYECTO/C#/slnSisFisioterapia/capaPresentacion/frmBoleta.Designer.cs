cce3af9b44badaa87449ce3b2a62ccce14109578
ALUMNO101 <ALUMNO101@ISBESRVL02M12>
2023-07-10 20:39:25 -0500

    ok

--- PROYECTO/C#/slnSisFisioterapia/capaPresentacion/frmBoleta.Designer.cs
+++ PROYECTO/C#/slnSisFisioterapia/capaPresentacion/frmBoleta.Designer.cs
@@ -297,6 +297,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(813, 491);
             Controls.Add(lbldescuento);
             Controls.Add(lblImpuesto);
